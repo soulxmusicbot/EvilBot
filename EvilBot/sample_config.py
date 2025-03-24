1654c2378a7b7f6aadeecc720cdb6dc106bc4165
soulxmusicbot <rustydragon20@gmail.com>
2025-03-24 10:04:28 +0530

    Update sample_config.py

--- EvilBot/sample_config.py
+++ EvilBot/sample_config.py
@@ -39,9 +39,9 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    DRAGONS = get_user_list("elevated_users.json", "7368341648 2107880985")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    DEV_USERS = get_user_list("elevated_users.json", "7368341648 2107880985")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
     DEMONS = get_user_list("elevated_users.json", "supports")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
